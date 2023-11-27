488fe93ba568de60196aed41f0b03fdab785f914
lRVASl <osddev2023@gmail.com>
2023-11-27 10:43:57 +0700

    update service member featur update del user

--- services/namecard/src/member/member.controller.ts
+++ services/namecard/src/member/member.controller.ts
@@ -6,8 +6,11 @@ import {
   Patch,
   Param,
   Delete,
+  UseInterceptors,
 } from '@nestjs/common';
-// import { AppErrorExceptionFilter } from '@namecard-lawyers/share';
+import { UserInterceptor } from '@namecard-lawyers/share';
+import { diskStorage } from 'multer';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { MemberService } from './member.service';
 import { Prisma } from '@prisma/client';
 import config from '../configs';
@@ -31,8 +34,31 @@ export class MemberController {
     return this.memberService.findByCondition(condition);
   }
 
+  @Post('/images/:codeNews')
+  @UseInterceptors(
+    FilesInterceptor('images', 10, {
+      storage: diskStorage({
+        destination: `${process.env.PATH_MOUNTDIR_IMAGES}`,
+        filename: (req, file, callback) => {
+          const filename = `${uuidv4()}-${file.originalname}`;
+          callback(null, filename);
+        },
+      }),
+    }),
+    UserInterceptor,
+  )
   @Get()
   findAll() {
     return this.memberService.findAll();
+  }
+
+  @Patch('/updateuser')
+  update(@Body('condition') condition: Prisma.namecardUpdateArgs) {
+    return this.memberService.update(condition);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return this.memberService.remove(id);
   }
 }
--- services/namecard/src/member/member.service.ts
+++ services/namecard/src/member/member.service.ts
@@ -10,7 +10,7 @@ export class MemberService {
     return this.namecardRepo.createMany(condition);
   }
 
-  async findByCondition(conditionscondition:any) {
+  async findByCondition(conditionscondition: any) {
     const conditions = {
       where: {
         id: Number(conditionscondition.id),
@@ -20,5 +20,18 @@ export class MemberService {
   }
   async findAll() {
     return this.namecardRepo.findAll();
+  }
+
+  async update(condition: Prisma.namecardUpdateArgs) {
+    return this.namecardRepo.update(condition);
+  }
+
+  async remove(id: number) {
+    const condition = {
+      where: {
+        id: Number(id),
+      },
+    } as Prisma.master_data_occupationDeleteArgs;
+    return this.namecardRepo.delete(condition);
   }
 }
